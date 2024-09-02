f5c6a724affcb83beb3267ee831129ce8ab64df6
tataganesh <tataganesh95@gmail.com>
2024-09-02 17:05:05 -0600

    FEAT: Add inital setup with stack implementation
    - precommit hook with linter
    - pyproject.toml for project and dependencies setup
    - basic CI workflow for testing

---
+++ dsa_implementations/data_structures/stack.py
@@ -0,0 +1,36 @@
+class Stack:
+
+    def __init__(self):
+        self.array = []
+
+    def push(self, x):
+        """Push element into stack"""
+        self.array.append(x)
+
+    def pop(self):
+        """Pop last inserted element from stack"""
+        if not self.array:
+            raise IndexError("Cannot pop from empty stack")
+        return self.array.pop()
+
+    def top(self):
+        """Get top (last inserted) element in stack"""
+        if not self.array:
+            raise IndexError("Cannot retrieve top element from empty stack")
+        return self.array[-1]
+
+    def size(self):
+        """Get size of stack"""
+        return len(self.array)
+
+    def isEmpty(self):
+        """Check if stack is empty"""
+        return not self.array
+
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(1)
+    print(f"Top Element: {s.top()}")  # Prints 1
+    print(f"Popped Element: {s.pop()}")  # Pops 1
+    s.top()  # Raises exception
---
+++ tests/test_stack.py
@@ -0,0 +1,89 @@
+import pytest
+from dsa_implementations.data_structures.stack import Stack
+
+
+@pytest.fixture
+def stack():
+    return Stack()
+
+
+def test_basic_push(stack):
+    stack.push(5)
+    assert stack.top() == 5
+    stack.push(-1)
+    assert stack.top() == -1
+
+
+def test_basic_pop(stack):
+    stack.push(5)
+    stack.push(2)
+    assert stack.pop() == 2
+    assert stack.pop() == 5
+    assert stack.isEmpty()
+
+
+def test_top(stack):
+    stack.push(5)
+    stack.push(10)
+    stack.push(15)
+    assert stack.top() == 15
+    stack.pop()
+    assert stack.top() == 10
+
+
+def test_isempty(stack):
+    stack.push(5)
+    stack.push(10)
+    stack.pop()
+    assert not stack.isEmpty()
+    stack.pop()
+    assert stack.isEmpty()
+
+
+def test_pop_emptystack(stack):
+    with pytest.raises(IndexError, match="Cannot pop from empty stack"):
+        stack.pop()
+
+
+def test_top_emptystack(stack):
+    with pytest.raises(
+        IndexError, match="Cannot retrieve top element from empty stack"
+    ):
+        stack.top()
+
+
+def test_multiple_data_types(stack):
+    stack.push(5)
+    stack.push("hello")
+    stack.push([1, 2, 3])
+    assert stack.pop() == [1, 2, 3]
+    assert stack.pop() == "hello"
+    assert stack.pop() == 5
+
+
+def test_stack_size(stack):
+    assert stack.size() == 0
+    stack.push(1)
+    assert stack.size() == 1
+    stack.push(10)
+    assert stack.size() == 2
+    stack.pop()
+    assert stack.size() == 1
+    stack.top()
+    assert stack.size() == 1
+
+
+@pytest.mark.parametrize(
+    "items",
+    [
+        [1, 2, 3, 4, 5],
+        ["abc", "def"],
+        [True, False, True, True, True],
+        [55, False, (2, 4, 6)],
+    ],
+)
+def test_stack_order(stack, items):
+    for item in items:
+        stack.push(item)
+    for item in reversed(items):
+        assert stack.pop() == item
